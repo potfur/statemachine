bc55a700c6c6f621d4b7d4c4af2063c60b5c0ea1
Potfur <wachowski.michal@gmail.com>
2015-04-06 13:17:33 +0200

    handles timeouts

--- src/StateMachine/StateMachine.php
+++ src/StateMachine/StateMachine.php
@@ -18,19 +18,33 @@ namespace StateMachine;
  */
 class StateMachine
 {
+    /**
+     * @var AdapterInterface
+     */
     private $adapter;
+
+    /**
+     * @var PayloadHandlerInterface
+     */
     private $payloadHandler;
+
+    /**
+     * @var TimeoutHandlerInterface
+     */
+    private $timeoutHandler;
 
     /**
      * Constructor
      *
      * @param AdapterInterface        $adapter
      * @param PayloadHandlerInterface $payloadHandler
+     * @param TimeoutHandlerInterface $timeoutHandler
      */
-    public function __construct(AdapterInterface $adapter, PayloadHandlerInterface $payloadHandler)
+    public function __construct(AdapterInterface $adapter, PayloadHandlerInterface $payloadHandler, TimeoutHandlerInterface $timeoutHandler)
     {
         $this->adapter = $adapter;
         $this->payloadHandler = $payloadHandler;
+        $this->timeoutHandler = $timeoutHandler;
     }
 
     /**
@@ -45,8 +59,59 @@ class StateMachine
      */
     public function triggerEvent($event, $identifier)
     {
+        $process = $this->adapter->getProcess();
         $payload = $this->payloadHandler->restore($identifier);
-        $result = $this->adapter->getProcess()->triggerEvent($event, $payload);
+
+        return $this->resolveEvent($process, $payload, $event);
+    }
+
+    /**
+     * Resolve timeouts
+     * Retrieves all expired timeouts and triggers proper events
+     * Return array of run histories
+     *
+     * @return array
+     */
+    public function resolveTimeouts()
+    {
+        $result = [];
+        $process = $this->adapter->getProcess();
+        $timeouts = $this->timeoutHandler->getExpired();
+
+        foreach ($timeouts as $timeout) {
+            $payload = $this->payloadHandler->restore($timeout->getIdentifier());
+
+            if ($payload->getState() !== $timeout->getState()) {
+                $this->timeoutHandler->remove($timeout);
+                continue;
+            }
+
+            $result[$timeout->getIdentifier()] = $this->resolveEvent($process, $payload, $timeout->getEvent());
+            $this->timeoutHandler->remove($timeout);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Execute event in set process for set payload
+     * If final state has timeout event, store it for further execution
+     * Return run history
+     *
+     * @param ProcessInterface $process
+     * @param PayloadInterface $payload
+     * @param string           $event
+     *
+     * @return array
+     */
+    private function resolveEvent(ProcessInterface $process, PayloadInterface $payload, $event)
+    {
+        $result = $process->triggerEvent($event, $payload);
+
+        if ($payload->hasChanged() && $process->hasTimeout($payload)) {
+            $this->timeoutHandler->store($process->getTimeout($payload, new \DateTime()));
+        }
+
         $this->payloadHandler->store($payload);
 
         return $result;
--- tests/StateMachine/StateMachineTest.php
+++ tests/StateMachine/StateMachineTest.php
@@ -34,23 +34,127 @@ class StateMachineTest extends \PHPUnit_Framework_TestCase
      */
     private $payloadHandler;
 
+    /**
+     * @var TimeoutHandlerInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $timeoutHandler;
+
     public function setUp()
     {
         $this->adapter = $this->getMock('\StateMachine\AdapterInterface');
+
         $this->process = $this->getMock('\StateMachine\ProcessInterface');
+
         $this->payload = $this->getMock('\StateMachine\PayloadInterface');
         $this->payloadHandler = $this->getMock('\StateMachine\PayloadHandlerInterface');
+
+        $this->timeoutHandler = $this->getMock('\StateMachine\TimeoutHandlerInterface');
     }
 
-    public function testTriggerEvent()
+    public function testTriggerEventWithoutTimeoutBecauseThereIsNoTimeout()
     {
+        $this->payload->expects($this->once())->method('hasChanged')->willReturn(true);
+
         $this->process->expects($this->once())->method('triggerEvent')->with('event', $this->payload);
+        $this->process->expects($this->once())->method('hasTimeout')->willReturn(false);
+
         $this->adapter->expects($this->once())->method('getProcess')->willReturn($this->process);
 
         $this->payloadHandler->expects($this->once())->method('restore')->willReturn($this->payload);
         $this->payloadHandler->expects($this->once())->method('store')->with($this->payload);
 
-        $machine = new StateMachine($this->adapter, $this->payloadHandler);
+        $this->timeoutHandler->expects($this->never())->method('store');
+
+        $machine = new StateMachine($this->adapter, $this->payloadHandler, $this->timeoutHandler);
         $machine->triggerEvent('event', 'identifier');
+    }
+
+    public function testTriggerEventWithoutTimeoutBecausePayloadDidNotChange()
+    {
+        $this->payload->expects($this->once())->method('hasChanged')->willReturn(false);
+
+        $this->process->expects($this->once())->method('triggerEvent')->with('event', $this->payload);
+
+        $this->adapter->expects($this->once())->method('getProcess')->willReturn($this->process);
+
+        $this->payloadHandler->expects($this->once())->method('restore')->willReturn($this->payload);
+        $this->payloadHandler->expects($this->once())->method('store')->with($this->payload);
+
+        $this->timeoutHandler->expects($this->never())->method('store');
+
+        $machine = new StateMachine($this->adapter, $this->payloadHandler, $this->timeoutHandler);
+        $machine->triggerEvent('event', 'identifier');
+    }
+
+    public function testTriggerEventWithTimeout()
+    {
+        $this->payload->expects($this->once())->method('hasChanged')->willReturn(true);
+
+        $timeout = $this->getMockBuilder('\StateMachine\Timeout')->disableOriginalConstructor()->getMock();
+
+        $this->process->expects($this->once())->method('triggerEvent')->with('event', $this->payload);
+        $this->process->expects($this->once())->method('hasTimeout')->willReturn(true);
+        $this->process->expects($this->once())->method('getTimeout')->willReturn($timeout);
+
+        $this->adapter->expects($this->once())->method('getProcess')->willReturn($this->process);
+
+        $this->payloadHandler->expects($this->once())->method('restore')->willReturn($this->payload);
+        $this->payloadHandler->expects($this->once())->method('store')->with($this->payload);
+
+        $this->timeoutHandler->expects($this->once())->method('store')->with($timeout);
+
+        $machine = new StateMachine($this->adapter, $this->payloadHandler, $this->timeoutHandler);
+        $machine->triggerEvent('event', 'identifier');
+    }
+
+    public function testResolveTimeoutsWithoutAny()
+    {
+        $this->timeoutHandler->expects($this->once())->method('getExpired')->willReturn([]);
+        $this->timeoutHandler->expects($this->never())->method('remove');
+
+        $this->payloadHandler->expects($this->never())->method('restore');
+
+        $this->process->expects($this->never())->method('triggerEvent');
+
+        $machine = new StateMachine($this->adapter, $this->payloadHandler, $this->timeoutHandler);
+        $machine->resolveTimeouts();
+    }
+
+    public function testResolveTimeoutsWithPayloadInInvalidState()
+    {
+        $timeout = $this->getMockBuilder('\StateMachine\Timeout')->disableOriginalConstructor()->getMock();
+        $timeout->expects($this->once())->method('getState')->willReturn('timeout');
+
+        $this->timeoutHandler->expects($this->once())->method('getExpired')->willReturn([$timeout]);
+        $this->timeoutHandler->expects($this->once())->method('remove');
+
+        $this->payload->expects($this->once())->method('getState')->willReturn('differentState');
+
+        $this->payloadHandler->expects($this->once())->method('restore')->willReturn($this->payload);
+
+        $this->process->expects($this->never())->method('triggerEvent');
+
+        $machine = new StateMachine($this->adapter, $this->payloadHandler, $this->timeoutHandler);
+        $machine->resolveTimeouts();
+    }
+
+    public function testResolveTimeoutsWithPayloadInCorrectState()
+    {
+        $timeout = $this->getMockBuilder('\StateMachine\Timeout')->disableOriginalConstructor()->getMock();
+        $timeout->expects($this->once())->method('getState')->willReturn('timeout');
+
+        $this->timeoutHandler->expects($this->once())->method('getExpired')->willReturn([$timeout]);
+        $this->timeoutHandler->expects($this->once())->method('remove');
+
+        $this->payload->expects($this->once())->method('getState')->willReturn('timeout');
+
+        $this->payloadHandler->expects($this->once())->method('restore')->willReturn($this->payload);
+
+        $this->adapter->expects($this->once())->method('getProcess')->willReturn($this->process);
+
+        $this->process->expects($this->once())->method('triggerEvent');
+
+        $machine = new StateMachine($this->adapter, $this->payloadHandler, $this->timeoutHandler);
+        $machine->resolveTimeouts();
     }
 }
