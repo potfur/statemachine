afabeeaf8e5c5f9ff8b636f9d2693af25556f4bc
Potfur <wachowski.michal@gmail.com>
2015-04-16 00:45:15 +0200

    facade

---
+++ tests/unit/StateMachine/Facade/FacadeTest.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+* This file is part of the statemachine package
+*
+* (c) Michal Wachowski <wachowski.michal@gmail.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace StateMachine\Facade;
+
+
+use StateMachine\StateMachine;
+
+class FacadeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var StateMachine|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $machine;
+
+    public function setUp()
+    {
+        $this->machine = $this->getMockBuilder('\StateMachine\StateMachine')->disableOriginalConstructor()->getMock();
+    }
+
+    public function testRegister()
+    {
+        $facade = new Facade();
+
+        $this->assertFalse($facade->has('foo'));
+
+        $facade->register('foo', function () { });
+
+        $this->assertTrue($facade->has('foo'));
+    }
+
+    public function testGet()
+    {
+        $facade = new Facade();
+        $facade->register('foo', function () { return $this->machine; });
+
+        $this->assertInstanceOf('\StateMachine\StateMachine', $facade->get('foo'));
+    }
+
+    /**
+     * @expectedException \StateMachine\Exception\InvalidArgumentException
+     * @expectedExceptionMessage State machine with name "foo" was not defined
+     */
+    public function testGetUnregistered()
+    {
+        $facade = new Facade();
+        $facade->get('foo');
+    }
+
+    public function testTriggerEvent()
+    {
+        $this->machine->expects($this->once())->method('triggerEvent')->with('bar', 1);
+        $facade = new Facade();
+        $facade->register('foo', function () { return $this->machine; });
+        $facade->triggerEvent('foo', 'bar', 1);
+    }
+
+    public function testResolveTimeoutsForAllMachines()
+    {
+        $this->machine->expects($this->exactly(2))->method('resolveTimeouts')->with();
+
+        $facade = new Facade();
+        $facade->register('foo', function () { return $this->machine; });
+        $facade->register('bar', function () { return $this->machine; });
+
+        $facade->resolveTimeouts();
+    }
+
+    public function testResolveTimeoutsForSetMachines()
+    {
+        $this->machine->expects($this->once())->method('resolveTimeouts')->with();
+
+        $facade = new Facade();
+        $facade->register('foo', function () { return $this->machine; });
+        $facade->register('bar', function () { return $this->machine; });
+
+        $facade->resolveTimeouts(['foo']);
+    }
+}
