c2314bfbd87525b1ee4523d6cb75a74b4b29aaa8
Potfur <wachowski.michal@gmail.com>
2015-05-12 23:30:14 +0200

    phpdoc + removed callable

--- src/StateMachine/Adapter/TimeoutConverter.php
+++ src/StateMachine/Adapter/TimeoutConverter.php
@@ -23,43 +23,34 @@ final class TimeoutConverter implements TimeoutConverterInterface
     /**
      * Convert different timeout formats to \DateTime or \DateInterval
      * Handles integers, strings and callable.
-     *
      * \DateInterval:
      *  - integers are treated as seconds,
      *  - strings starting with P are treated as ISO 8601 durations
-     *
      * \DateTime:
      *  - other strings will be treated as date time format
      *
      * @see http://en.wikipedia.org/wiki/ISO_8601#Durations
      * @see http://php.net/manual/en/datetime.formats.php
+     *
+     * @param mixed $value
+     *
      * @return \DateTime|\DateInterval
      */
     public function convert($value)
     {
         if ($value instanceof \DateTime || $value instanceof \DateInterval) {
             return $value;
-        } elseif (filter_var($value, FILTER_VALIDATE_INT)) {
-            return $this->convertInteger($value);
-        } elseif (is_callable($value)) {
-            return $this->convertCallable($value);
-        } elseif (strpos($value, 'P') === 0) {
-            return $this->convertDuration($value);
-        } else {
-            return $this->convertDate($value);
         }
-    }
 
-    /**
-     * Convert integer to \DateInterval
-     *
-     * @param int $int
-     *
-     * @return \DateInterval
-     */
-    private function convertInteger($int)
-    {
-        return $this->convertDuration(sprintf('PT%uS', $int));
+        if (filter_var($value, FILTER_VALIDATE_INT)) {
+            return $this->convertDuration(sprintf('PT%uS', $value));
+        }
+
+        if (strpos($value, 'P') === 0) {
+            return $this->convertDuration($value);
+        }
+
+        return $this->convertDate($value);
     }
 
     /**
@@ -84,17 +75,5 @@ final class TimeoutConverter implements TimeoutConverterInterface
     private function convertDate($date)
     {
         return new \DateTime($date);
-    }
-
-    /**
-     * Call callable and convert value
-     *
-     * @param callable $callable
-     *
-     * @return \DateInterval|\DateTime
-     */
-    private function convertCallable(callable $callable)
-    {
-        return $this->convert(call_user_func($callable));
     }
 }
